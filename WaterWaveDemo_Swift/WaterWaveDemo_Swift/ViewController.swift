5bad37e1acdf83974de321897a85535ea7362308
Antonio081014 <antonio081014@antonio081014.com>
2016-09-19 00:26:18 -0700

    Add Demo in Swift.

---
+++ WaterWaveDemo_Swift/WaterWaveDemo_Swift/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  WaterWaveDemo_Swift
+//
+//  Created by Antonio081014 on 9/18/16.
+//  Copyright © 2016 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var wave = SPWaterProgressIndicatorView.init(frame: .zero)
+    var isAnimating: Bool = false {
+        didSet{
+            if isAnimating {
+                self.wave.startAnimation()
+            } else {
+                self.wave.stopAnimation()
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.wave = SPWaterProgressIndicatorView(frame: self.view.bounds)
+        self.wave.center = self.view.center;
+        self.view.addSubview(self.wave)
+        
+        let tap = UITapGestureRecognizer.init(target: self, action: #selector(ViewController.updateAnimation))
+        self.wave.addGestureRecognizer(tap)
+        
+        let pan = UIPanGestureRecognizer.init(target: self, action: #selector(ViewController.updatePercent))
+        self.wave.addGestureRecognizer(pan)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+     func updatePercent(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .changed || gesture.state == .ended {
+            let location = gesture.location(in: self.wave)
+            let percent = 100 - Int(100.0 * location.y / self.wave.bounds.height);
+            self.wave.completionInPercent = percent
+        }
+    }
+
+     func updateAnimation() {
+        self.isAnimating = !self.isAnimating
+    }
+}
+
