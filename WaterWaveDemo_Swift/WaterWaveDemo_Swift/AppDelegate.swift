0ea020daf5ba07f80d922c85c62146b41f4d4c00
Antonio081014 <antonio081014@antonio081014.com>
2016-09-19 09:34:20 -0700

    Change Project compatible with iOS 8.0+.

--- WaterWaveDemo_Swift/WaterWaveDemo_Swift/AppDelegate.swift
+++ WaterWaveDemo_Swift/WaterWaveDemo_Swift/AppDelegate.swift
@@ -41,11 +41,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+        if #available(iOS 10.0, *) {
+            self.saveContext()
+        } else {
+            // Fallback on earlier versions
+        }
     }
 
     // MARK: - Core Data stack
 
+    @available(iOS 10.0, *)
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -75,6 +80,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
+    @available(iOS 10.0, *)
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
